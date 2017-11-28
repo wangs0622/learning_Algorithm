e459c5bcdd373e48df2c1487d3758d34c819e8ba
wangs0622 <wangs0622@126.com>
2017-11-28 13:40:01 +0800

    Create simpleLink.py

---
+++ simpleLink.py
@@ -0,0 +1,141 @@
+# _*_ encoding:utf-8 _*_
+
+'''
+Created on 2017/11/28
+
+@author: wangs0622
+version: python2.7
+'''
+
+
+# 定义节点类，Node
+
+class Node():
+     
+    def __init__(self, data = None, nnext = None):
+        self.data = data
+        self.next = nnext
+        
+    def __repr__(self):
+        return 'data: ' + str(self.data)
+    
+# 定义链表类，link
+
+class Link():
+    
+    def __init__(self):
+        self.first = None
+        self.length = 0
+        
+    def isEmpty(self):
+        '''
+        judge this is empty or not
+        '''
+        return self.length == 0
+    
+    def appendFirst(self, node):
+        '''
+        add a node at the first of the link
+        '''
+        if self.isEmpty():
+            self.first = node
+            self.length = 1
+        else:
+            old_first = self.first
+            self.first = node
+            self.first.next = old_first
+            self.length += 1
+    
+    def dealFirst(self):
+        '''
+        deal the first node of the link
+        '''
+        if self.isEmpty():
+            print "This link is empty"
+        else:
+            self.first = self.first.next
+            self.length -= 1
+        
+    def size(self):
+        '''
+        get the length of the link
+        '''
+        return self.length
+    
+    def __iter__(self):
+        temp = self.first
+        while True:
+            yield temp
+            if temp.next == None:
+                break
+            else: 
+                temp = temp.next
+        
+    def __repr__(self):
+        '''
+        print this link
+        '''
+        if self.isEmpty(): 
+            return ""
+        else:
+            s = ''
+            for node in self:
+                s = s + str(node.data) + ' -> '
+            return s[:-4]
+                                      
+if __name__ == '__main__':
+ 
+    link = Link()
+    link.appendFirst(Node(data = 'first'))
+    link.appendFirst(Node(data = 'second'))
+    link.appendFirst(Node(data = 'third'))
+    link.appendFirst(Node(data = 'fourth'))
+    link.appendFirst(Node(data = 'fifth'))
+    link.appendFirst(Node(data = 'sixth'))
+    
+    print "link: ", link
+    print "link length: ", link.length
+    
+    
+    link.dealFirst()
+
+    print "link: ", link
+    print "link length: ", link.length
+
+    
+    link.dealFirst()
+
+    print "link: ", link
+    print "link length: ", link.length
+    
+    link.dealFirst()
+
+    print "link: ", link
+    print "link length: ", link.length
+    
+    link.dealFirst()
+
+    print "link: ", link
+    print "link length: ", link.length
+    
+    link.dealFirst()
+    print "link: ", link
+    print "link length: ", link.length
+    
+    link.dealFirst()
+    print "link: ", link
+    print "link length: ", link.length
+
+    link.dealFirst()
+    print "link: ", link
+    print "link length: ", link.length
+    
+    link.dealFirst()
+    print "link: ", link
+    print "link length: ", link.length
+    
+    link.dealFirst()
+    print "link: ", link
+    print "link length: ", link.length
+    
+    
