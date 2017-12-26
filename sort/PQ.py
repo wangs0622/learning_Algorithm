47451084631a2672130d9a6bb2cb00c8368d0a04
wangs0622 <wangs0622@126.com>
2017-12-26 16:36:57 +0800

    add sort/PQ.py file and modify sort/readme.md by adding some comment about PQ.py file

---
+++ sort/PQ.py
@@ -0,0 +1,74 @@
+# _*_ encoding: utf-8 _*_
+
+import queue
+from maxPQ import printHeap
+
+minPQ = queue.PriorityQueue
+
+
+def heappush(heap, item):
+    heap.append(item)
+    _siftdown(heap, startpos = 0, pos = len(heap)-1)
+    
+def heappop(heap):
+    lastelt = heap.pop()
+    if heap:
+        returnitem = heap[0]
+        heap[0] = lastelt
+        _siftup(heap, 0)
+        return returnitem
+    return lastelt
+    
+def _siftdown(heap, startpos, pos):
+    newitem = heap[pos]
+    while pos > startpos:
+        parentpos = (pos - 1) >> 1
+        parent = heap[parentpos]
+        if newitem > parent:
+            heap[pos] = parent
+            pos = parentpos
+            continue
+        break
+    heap[pos] = newitem
+    
+def _siftup(heap, pos):
+    endpos = len(heap)
+    startpos = pos
+    newitem = heap[pos]
+    childpos = 2*pos + 1    # leftmost child position
+    while childpos < endpos:
+        # Set childpos to index of smaller child.
+        rightpos = childpos + 1
+        if rightpos < endpos and  heap[childpos] < heap[rightpos]:
+            childpos = rightpos
+        # Move the smaller child up.
+        heap[pos] = heap[childpos]
+        pos = childpos
+        childpos = 2*pos + 1
+    # The leaf at pos is empty now.  Put newitem there, and bubble it up
+    # to its final resting place (by sifting its parents down).
+    heap[pos] = newitem
+    _siftdown(heap, startpos, pos)
+
+class MaxPQ(queue.Queue):
+    def _init(self, maxsize):
+        self.queue = []
+
+    def _qsize(self):
+        return len(self.queue)
+
+    def _put(self, item):
+        heappush(self.queue, item)
+
+    def _get(self):
+        return heappop(self.queue)
+    
+if __name__ == '__main__':
+    items = 'qwertyuiopasdfghjklzxcvbnm'
+    maxpq = MaxPQ()
+    for x in items:
+        maxpq.put(x)
+    print(maxpq.queue)
+    printHeap(maxpq.queue)
+     
+
--- sort/readme.md
+++ sort/readme.md
@@ -23,17 +23,37 @@
 | mergeSortWithPrimary() | 改进方法1 的归并排序 |
 | mergeSortWithPrimaryAndJudge() | 改进方法1+2 的归并排序 |
 
+------
+
 - `maxPQ.py` 介绍优先队列以及堆操作，此文件的运行环境为 python 3.6.4
+
+具体内容参见： [http://www.wangs0622.com/2017/12/26/PrivorityQueueAndHeap/](http://www.wangs0622.com/2017/12/26/PrivorityQueueAndHeap/)
 
 | 函数名 | 函数作用 |
 |-------| --------|
-| class maxPQ() | 创建无序 deque 的优先队列 |
+| class MaxPQ() | 创建无序 deque 的优先队列 |
 | class MaxPQbasedHeap() | 创建基于堆操作的优先队列 |
 | swim() | 实现堆操作的上浮操作 |
 | sink() | 实现堆操作的下沉操作 |
 | printHeap() | 以可视化的方式显示堆中的数据 |
 
+------
 
+- `PQ.py` 介绍基于 Python 内置优先队列类与内置的堆操作函数而改进后优先队列，此文件的运行环境为 python 3.6.4
 
+Python 自带的优先队列应该算的上是 MinPQ 类，即堆中最小元素位于首位，且从第零个元素开始存储数据。
+
+在此文件中，我们基于 Python 自带的优先队列，定义了一个 MaxPQ 类，即最大元素位于首部，从零位开始存储数据。
+
+具体内容参见： [http://www.wangs0622.com/2017/12/26/PrivorityQueueAndHeap/](http://www.wangs0622.com/2017/12/26/PrivorityQueueAndHeap/)
+
+| 函数/类 名 | 函数作用 |
+|-------| --------|
+| class MinPQ() | Python 内置的优先队列 |
+| class MaxPQ() | 基于内置优先队列改变后的优先队列 |
+| heapush(heap, item) | 将元素 item 添加到 heap 这个堆结构中 |
+| heappop(heap) | 获取 heap 这个堆结构中最大的元素 |
+| _siftdown(heap, startpos, pos) | 将位于 pos 位，一般是末尾，的元素进行下沉操作 |
+| _siftup(heap, pos) | 将位于 pos 位，一般是第零位，的元素进行上浮操作 |
 
 
